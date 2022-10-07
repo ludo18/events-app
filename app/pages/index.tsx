394f91c8365fddda607d341afcd7779223a91cdb
BERGSON\ludo <ldutat@outlook.com>
2022-10-07 17:18:41 -0400

    home background image

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,15 @@
 import Layout from '@/components/layout';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Image from 'next/image';
 
 export default function Home(): React.ReactElement {
   const { t } = useTranslation('common');
   return (
     <Layout title={t('Home')} showHeader={true} showFooter={true}>
+      <div className="mainimg">
+        <Image src="/images/sekandeya.webp" width={1900} height={300} />
+      </div>
       <h1>{t('home.PowerPhrase')}</h1>
       <h2>{t('home.SubPowerPhrase')}</h2>
       <div>{t('home.PowerText')}</div>
